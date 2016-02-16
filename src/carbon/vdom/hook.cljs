9b7ab95a23d9b4bdd7528764c568c53d42889bab
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-02-16 10:28:06 +0200

    minor fix

--- src/carbon/vdom/hook.cljs
+++ src/carbon/vdom/hook.cljs
@@ -4,9 +4,9 @@
 
   Object
   (hook [this node prop prev]
-    (h this node prop prev))
+    (when h (h this node prop prev)))
   (unhook [this node prop next]
-    (u this node prop next))
+    (when u (u this node prop next)))
 
   IDeref
   (-deref [_]
@@ -32,14 +32,11 @@
 (goog/exportSymbol "Hook.prototype.hook" Hook.prototype.hook)
 (goog/exportSymbol "Hook.prototype.unhook" Hook.prototype.unhook)
 
-
-(def noop (constantly nil))
-
 (defn hook
-  ([h] (hook h noop nil))
+  ([h] (hook h nil nil))
   ([h u] (hook h u nil))
   ([h u a] (Hook. h u (atom a))))
 
 (defn unhook
-  ([u] (hook noop u nil))
-  ([u a] (hook noop u a)))
+  ([u] (hook nil u nil))
+  ([u a] (hook nil u a)))
