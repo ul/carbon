e97d0e8677d27ab20a658870a7633464323cc169
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-02-16 16:48:33 +0200

    up deps and fix typo

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.189"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/core.async "0.2.374"]]
 
   :plugins [[lein-cljsbuild "1.1.2"]
-            [lein-figwheel "0.5.0-2"]]
+            [lein-figwheel "0.5.0-6"]]
 
   :source-paths ["src"]
 
