e3b833d0775738be90542a4a902cb1ba989f5bd5
Ruslan Prokopchuk <fer.obbee@gmail.com>
2016-06-15 19:43:47 +0300

    up to carbon.rx 0.1.4

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.36"]
-                 [carbon/rx "0.1.3"]]
+                 [carbon/rx "0.1.4"]]
 
   :source-paths ["src"])
