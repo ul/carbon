cbdafb24d6dc9abf403e98d9d1417812ba41402a
Ruslan Prakapchuk <Ruslan.Prakapchuk@AudienceScience.com>
2016-09-29 14:43:40 +0300

    up deps

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.36"]
-                 [carbon/rx "0.1.9"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [carbon/rx "0.2.0-SNAPSHOT"]]
 
   :source-paths ["src"])
