7f94e9b974358eb7e5496ffbb70d00e0119973c9
Ruslan Prokopchuk <fer.obbee@gmail.com>
2017-03-14 18:11:12 +0200

    update deps

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.456"]
-                 [cljsjs/inferno "1.2.2-0"]
+                 [org.clojure/clojurescript "1.9.495"]
+                 [cljsjs/inferno "1.4.0-0"]
                  [carbon/rx "0.3.0-SNAPSHOT"]]
 
   :source-paths ["src"])
