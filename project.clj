4a48c3c849dd270824874a1d9c7e4cc22b03aa5d
Ruslan Prakapchuk <fer.obbee@gmail.com>
2026-01-22 21:59:22 +1100

    up deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.10.0-alpha8"]
-                 [org.clojure/clojurescript "1.10.339"]
+  :dependencies [[org.clojure/clojure "1.12.4"]
+                 [org.clojure/clojurescript "1.12.134"]
                  [cljsjs/inferno "5.6.0-0"]
-                 [carbon/rx "0.3.0-SNAPSHOT"]]
+                 [carbon/rx "0.4.0-SNAPSHOT"]]
 
   :source-paths ["src"])
